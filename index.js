fd7ef63238156a154bc13783e56199e438605d6f
deathworship <125945145+deathworship@users.noreply.github.com>
2023-09-07 21:46:32 +0300

    3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -50,6 +52,19 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    body.id = generateId()
+
+    persons = persons.concat(body)
+
+    response.json(body)
 })
 
 const PORT = 3001
