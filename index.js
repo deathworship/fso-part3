afcbe4f0a4417dd82ec4cc7b2df08c7aeebf5400
deathworship <125945145+deathworship@users.noreply.github.com>
2023-09-09 06:30:57 +0300

    3.11 phonebook full stack

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('reqbody', (req, res) => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqbody'))
