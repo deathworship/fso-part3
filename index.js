298d0157a89c396f364e17e569100cc287e67654
deathworship <125945145+deathworship@users.noreply.github.com>
2023-09-11 07:26:08 +0300

    3.18*: Phonebook database step6

--- index.js
+++ index.js
@@ -43,26 +43,30 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    const page = `<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>` 
-    response.send(page)
+    Person.countDocuments({}).then(count => {
+        const page = `<p>Phonebook has info for ${count} people</p><p>${Date()}</p>` 
+        response.send(page)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    console.log(person)
-    if (person) {
-        response.json(person)
-    }
-    else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            }
+            else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
     .then(result => {
-        response.status(204).end()
+        response.status(200).end()
     })
     .catch(error => next(error))
 })
