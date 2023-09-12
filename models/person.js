6fe40d896137b851450814bf3c424741727dca12
deathworship <125945145+deathworship@users.noreply.github.com>
2023-09-12 16:06:26 +0300

    3.22: Lint configuration

--- models/person.js
+++ models/person.js
@@ -7,37 +7,37 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => { 
-        console.log('error connecting to MongoDB:', error.message) 
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const schema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d+/.test(v)
+      }
     },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: (v) => {
-                return /^\d{2,3}-\d+/.test(v)
-            }
-        },
-        required: true
-    }
+    required: true
+  }
 })
 
 schema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-  module.exports = mongoose.model('Person', schema)
+module.exports = mongoose.model('Person', schema)
