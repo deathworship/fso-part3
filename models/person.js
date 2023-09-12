c8055e3e01e6d6a1ff7f295e2c4a454eada9faf1
deathworship <125945145+deathworship@users.noreply.github.com>
2023-09-12 15:16:51 +0300

    3.20*: Phonebook database, step8

--- models/person.js
+++ models/person.js
@@ -22,6 +22,12 @@ const schema = new mongoose.Schema({
     },
     number: {
         type: String,
+        minLength: 8,
+        validate: {
+            validator: (v) => {
+                return /^\d{2,3}-\d+/.test(v)
+            }
+        },
         required: true
     }
 })
